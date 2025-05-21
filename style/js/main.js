0aeb20357d5c76595e65f111d3c6a1367475d374
SahanRansika <sahanransika07@gmail.com>
2025-05-21 15:01:38 +0530

    Add To Scroll Section Active Link - js

--- style/js/main.js
+++ style/js/main.js
@@ -20,3 +20,23 @@ tabs.forEach(tab => {
     })
 })
 
+// Scroll Section Active Link
+
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', navHighlighter);
+
+function navHighlighter() {
+    let scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
