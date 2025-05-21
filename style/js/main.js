39021cf99dbbd9edd707aa879b0d73fcbfdfce07
SahanRansika <sahanransika07@gmail.com>
2025-05-21 15:02:21 +0530

    Add To Activating Sidebar - js

--- style/js/main.js
+++ style/js/main.js
@@ -40,3 +40,21 @@ function navHighlighter() {
         }
     })
 }
+
+// Activating Sidebar
+
+const navMenu = document.getElementById('sidebar');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-sidebar');
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-sidebar');
+    })
+}
