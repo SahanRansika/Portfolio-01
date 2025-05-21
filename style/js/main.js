6ce9c2d5d0398b82a3b62d651a3908a4618a3b6a
SahanRansika <sahanransika07@gmail.com>
2025-05-21 15:10:21 +0530

    Add To Portfolio Popup - js

--- style/js/main.js
+++ style/js/main.js
@@ -40,6 +40,27 @@ function activeWork() {
 }
 linkWork.forEach(l => l.addEventListener('click', activeWork));
 
+//Portfolio Popup
+
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('work-button')){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector('.portfolio-popup').classList.toggle('open');
+}
+
+document.querySelector('.portfolio-popup-close').addEventListener('click', togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector('.pp-thumbnail img').src = portfolioItem.querySelector('.work-img').src;
+    document.querySelector('.portfolio-popup-subtitle span').innerHTML = portfolioItem.querySelector('.work-title').innerHTML;
+    document.querySelector('.portfolio-popup-body').innerHTML = portfolioItem.querySelector('.portfolio-item-details').innerHTML;
+}
+
 // Scroll Section Active Link
 
 const sections = document.querySelectorAll('section[id]');
